5043823b16cc42bd0d9759ee145cfeb9b5d3f2d3
yuan1202 <li_yuan@outlook.com>
2020-12-17 11:27:11 +0000

    resotre setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name         = 'Kheiron_Calculator',
     version      = '0.1',
     packages     = find_packages(),
+    install_requires=[
+        'flask>=1.1.2'
+    ],
     entry_points = {'console_scripts': ['kheiron_calc = calculators.calculators:calc', 'kheiron_calc_app = calculator_app.app:launch']},
 )
